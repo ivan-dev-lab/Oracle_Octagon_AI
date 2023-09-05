e2763fe23d0a26681a3f1b45f31dfa5eda6ac7b0
ivan-dev-lab <roalivan4@gmail.com>
2023-09-05 19:34:06 +0500

    исправлены ошибки, улучшен код

--- parser/parse_fights.py
+++ parser/parse_fights.py
@@ -6,7 +6,6 @@ from parse_fighters import parse_fighter
 
 MAIN_URL = "https://ufc.ru"
 EVENTS_URL = "https://ufc.ru/events"
-FIGHTERS_DF = pd.read_csv("data/fighters.csv", index_col=[0])
 
 # на сайте представлена такая структура: 1 event = ссылка на чемпионат с множеством боев, поэтому парсятся сначала event
 def parse_events () -> list:
@@ -25,7 +24,7 @@ def parse_events () -> list:
             if link.findNext("span").get_text().strip().lower() == "итоги":
                 Urls.append(MAIN_URL+link.get("href"))   
 
-def check_fighters (names: list[str], urls: list[str]) -> tuple[list, list]:
+def check_fighters (fighters_df: pd.DataFrame, names: list[str], urls: list[str]) -> tuple[list, list]:
     # В ID_list добавляются идентификаторы бойцов, которые нашлись в базе
     # В URL_list добавляются ссылки на тех бойцов, которых в базе нет
     # ссылки из URL_list будут парситься отдельно функцией parse_one_fighter() из parse_fighters.py
@@ -34,7 +33,7 @@ def check_fighters (names: list[str], urls: list[str]) -> tuple[list, list]:
     URL_list = []
 
     for index, name in enumerate(names):
-        FighterID = FIGHTERS_DF[FIGHTERS_DF["Name"] == name]["FighterID"].values
+        FighterID = fighters_df[fighters_df["Name"] == name]["FighterID"].values
     
         if FighterID.size > 0:             
             ID_list.append(FighterID[0])
@@ -63,25 +62,34 @@ def get_fighter_bio (url: str) -> dict:
     except: return {}
 
 
-def parse_fights ():
+def parse_fights () -> list():
     LINKS_EVENTS = [] # parse_events()  
     # убрать после завершения работы    
     with open("events.txt", "r", encoding="utf8") as f:
         for line in f.readlines():
             LINKS_EVENTS.append(line.strip())
-
-    for url in LINKS_EVENTS:
-        response = requests.get(url)
+    result = [] # массив для возврата бойцов, которых добавили в базу и истории боёв
+    fighters_df = pd.read_csv("data/fighters.csv", index_col=[0])
+    fighters = []
+    fight_cards = []
+    for index,event_url in enumerate(LINKS_EVENTS):
+        print(f"{index}/{len(LINKS_EVENTS)}")
+        response = requests.get(event_url)
         soup = BeautifulSoup(response.text, "html.parser")
 
         fight_cards_ResultSet = soup.find(name="ul", attrs={"class": "l-listing__group--bordered"}).find_all(name="li")
 
         for i, card in enumerate(fight_cards_ResultSet):
-            fight_card = {}
+            fight_card = {
+                "FightID": randint(100000,999999),
+                "URL": event_url 
+            }
+            # в данном случае card - карточка 1 боя
+
             # парсинг имён двух бойцов. Далее по ним ведется поиск в fighters.csv 
             # -> если находится - возвращает FighterID
             # -> если не находится, то пытается спарсить данные бойца. 
-            # Если получается -> заносит бойца в fighters.csv и fighters_data.csv -> возвращает FighterID
+            # Если получается -> заносит бойца в fighters.csv и fighters_data.csv -> возвращает FighterID            
             fighter_names = []
             fighter_names.append(soup.find_all(name="div", attrs={"class": "c-listing-fight__corner-name--red"})[i].get_text().strip().replace("\n", " "))
             fighter_names.append(soup.find_all(name="div", attrs={"class": "c-listing-fight__corner-name--blue"})[i].get_text().strip().replace("\n", " "))
@@ -89,45 +97,59 @@ def parse_fights ():
             fighter_urls = []
             fighter_urls.append(soup.find_all(name="div", attrs={"class": "c-listing-fight__corner-name--red"})[i].find(name="a").get("href"))
             fighter_urls.append(soup.find_all(name="div", attrs={"class": "c-listing-fight__corner-name--blue"})[i].find(name="a").get("href"))
-
-            check_result = check_fighters(fighter_names, fighter_urls)
-
+            
+            check_result = check_fighters(fighters_df, fighter_names, fighter_urls)
+            
+            
             # если в массиве с именами бойцов в бою 2 FighterID -> все бойцы найдены в базе
             if len(check_result[0]) == 2: 
                 fight_card["FighterID_1"] = check_result[0][0]
                 fight_card["FighterID_2"] = check_result[0][1]
             else:
-                fighters = []
                 # если нет, то проверяется сначала хотя бы один FighterID -> с помощью которого ищется ссылка на него, 
                 # а потом парсятся все ссылки из второго массива
                 # это сделано с той целью, чтобы не потерять изначальный порядок бойцов. 
                 # Пример: было: ([882259], ["https://ufc.ru/athlete/rayan-spenn"], 
                 # стало: ["https://ufc.ru/athlete/volkan-ozdemir", "https://ufc.ru/athlete/rayan-spenn"])
                 if len(check_result[0]) == 1:
-                    url = FIGHTERS_DF[FIGHTERS_DF["FighterID"] == check_result[0][0]]["URL"].values[0]
+                    url = fighters_df[fighters_df["FighterID"] == check_result[0][0]]["URL"].values[0]
                     check_result[0].clear()
                     check_result[1].append(url)
                     check_result[1].reverse() # reverse для того, чтобы сохранился порядок бойцов
                 # порядок бойцов нужен для того, чтобы в столбцах Result_1 и Result_2 писался исход боя для каждого
                 
-                fighters_df = pd.read_csv("data/fighters.csv", index_col=[0])
-                for url in check_result[1]:
+                
+                for i,url in enumerate(check_result[1], start=1):
                     if fighters_df[fighters_df["URL"] == url].values.size > 0:
-                        print(f"{url} - боец уже есть в базе")
+                        fight_card[f"FighterID_{i}"] = fighters_df[fighters_df["URL"] == url]["FighterID"].values
+                        # print(f"{url} - боец уже есть в базе")
                         continue
                     
                     fighter_bio = get_fighter_bio(url)
-                    if fighter_bio == {}: continue
+                    if fighter_bio == {}: 
+                        fight_card.clear()
+                        continue
                     
                     fighter = get_fighter_bio(url)
-
+                    
                     fighter_data = parse_fighter(url)
-                    if fighter_data == {}: continue
+                    if fighter_data == {}: 
+                        fight_card.clear()
+                        continue
 
                     fighter.update(fighter_data)
+                    fight_card[f"FighterID_{i}"] = fighter["FighterID"]
+
                     fighters.append(fighter)
                 
-                
-                pd.concat([fighters_df, pd.DataFrame(data=fighters)], ignore_index=True).to_csv("data/fighters.csv")
+            fight_card[f"Result_1"] = card.find(name="div", attrs={"class": "c-listing-fight__corner-body--red"}).get_text().strip().replace("\n", " ")
+            fight_card[f"Result_2"] = card.find(name="div", attrs={"class": "c-listing-fight__corner-body--blue"}).get_text().strip().replace("\n", " ")
             
-parse_fights ()
+            if len(fight_card) == 6:
+                # именно 6, потому что карточка боя с 6 параметрами включает в себя: FightID, URL, 
+                # FighterID_1, FighterID_2, Result_1, Result_2
+                fight_cards.append(fight_card)
+    
+    result.append(pd.concat([fighters_df, pd.DataFrame(data=fighters)], ignore_index=True))        
+    result.append(pd.DataFrame(data=fight_cards))
+    return result
