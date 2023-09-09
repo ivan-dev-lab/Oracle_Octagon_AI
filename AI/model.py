c1bf77529702edcf1b735e5eee99bc62a4e6732b
ivan-dev-lab <roalivan4@gmail.com>
2023-09-09 14:33:31 +0500

    добавлены коллбеки

--- AI/model.py
+++ AI/model.py
@@ -1,7 +1,13 @@
-import tensorflow as tf
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import Model, layers
 from keras.models import Sequential
 from keras.optimizers import Adam
+
+def callbacks () -> list:
+    early_stopping = EarlyStopping(monitor="val_accuracy", min_delta=0.01, patience=7, restore_best_weights=True)
+    model_checkpoint = ModelCheckpoint(filepath="model/", monitor="val_accuracy", save_best_only=True, save_weights_only=True)
+
+    return [early_stopping, model_checkpoint]
 
 def create_model (input_shape: int) -> Model:
     model = Sequential()
@@ -14,5 +20,4 @@ def create_model (input_shape: int) -> Model:
     model.add(layers.Dense(units=2, activation="softmax"))
 
     model.compile(optimizer=Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
-
     return model
