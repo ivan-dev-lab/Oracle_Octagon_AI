eb551fea3be197e5bee6c4b427808ba5969c1cd2
ivan-dev-lab <roalivan4@gmail.com>
2023-09-09 14:24:12 +0500

    убраны вызовы функций

--- AI/model.py
+++ AI/model.py
@@ -2,14 +2,6 @@ import tensorflow as tf
 from keras import Model, layers
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.model_selection import train_test_split
-from preprocess import preprocess
-
-early_stopping = EarlyStopping(monitor="val_accuracy", min_delta=0.01, patience=7, restore_best_weights=True)
-model_checkpoint = ModelCheckpoint(filepath="model/", monitor="val_accuracy", save_best_only=True, save_weights_only=True)
-
-callbacks = [early_stopping, model_checkpoint]
 
 def create_model (input_shape: int) -> Model:
     model = Sequential()
@@ -24,20 +16,3 @@ def create_model (input_shape: int) -> Model:
     model.compile(optimizer=Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
 
     return model
-
-X,Y = preprocess(fp_fighters="data/fighters.csv", fp_fights="data/fights.csv")
-x_train, x_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state=42)
-
-model = create_model(X.shape[1])
-model.fit(x_train,
-          y_train, 
-          batch_size=32, 
-          epochs=20, 
-          validation_data=(x_test, y_test),
-          # callbacks=callbacks,
-          )
-
-
-model.load_weights("model/")
-score = model.evaluate(x_test, y_test)
-print(score[1]) # 0.72
